9f0212ec3e62bbebf4c2ab1bb75034fa14a392dc
Jannik Lindemeier <jannik@lindemeier-online.de>
2022-01-19 08:43:29 +0100

    remove report function and add welcome message

--- setup.ps1
+++ setup.ps1
@@ -9,26 +9,16 @@ $Parent = $Home
 $Base = $Parent + "\" + $DirName
 $StarterRepo = "https://github.com/JannikLindemeier/gbs-aen-expressjs-starter.git"
 $Proxy = "http://192.168.100.1:8080"
-$ReportFile = "Z:\lindemeier_setupscript\$env:UserName.txt"
 
 # save current location
 $Loc = (Get-Location)
 
-# report function
-function Report($status){
-    try {
-        echo "$(Get-Date) - $status" >> $ReportFile
-    } catch {
-        Write-Output "Unable to send report '$status' to $ReportFile"
-    }
-}
-
-Report("init")
+Write-Output "Willkommen! Das nachfolgende Script wird den Proxy für NPM und Git konfigurieren, das Starter-Repository von $StarterRepo in $Base clonen, und anschließend die NodeJS-Abhängigkeiten installieren"
+Write-Output ""
 
 # check if base dir already exits
 if (Test-Path -Path $Base) {
     Write-Output "Verzeichnis $Base existiert bereits und kann nicht als Projektbasis verwendet werden"
-    Report("--EXIT: conflict--")
     Read-Host
     Exit
 }
@@ -38,32 +28,26 @@ Write-Output "Erstelle Projektverzeichnis: $Base"
 New-Item -ItemType "directory" -Path $Parent -Name $DirName | Out-Null # execute silently
 #...and cd into that directory in order to work with it
 Set-Location -Path $Base
-Report("dir: ok")
 
 # set npm proxy config
 Write-Output "Konfiguriere NPM-Proxy..."
 npm config set proxy $Proxy
-Report("npm proxy: ok")
 
 # Configure git proxy
 Write-Output "Konfiguriere Git-Proxy..."
 git config --global http.proxy $Proxy
-Report("git: ok")
 
 # Clone starter project
 Write-Output "Starter-Projekt herunterladen von $StarterRepo..."
 git clone $StarterRepo $Base
-Report("clone: ok")
 
 # install npm deps
 Write-Output "Installiere Projektabhängigkeiten..."
 npm install
-Report("npm deps: ok")
 
 # open explorer and vscode
 explorer $Base
 code -n $Base
-Report("window assist: ok")
 
 # leave project dir and return to old location
 Set-Location $Loc
@@ -71,6 +55,5 @@ Set-Location $Loc
 # exit
 Write-Output "Alles erledigt, das Projekt befindet sich in: $Base"
 # report success
-Report("--EXIT: success--")
 Read-Host "Beliebige Taste druecken, um dieses Fenster zu schließen"
 Exit
